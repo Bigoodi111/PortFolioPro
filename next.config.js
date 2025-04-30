e77e64646ab3e62e3279f0d79e015a508e154f42
Bigoodi111 <144342508+Bigoodi111@users.noreply.github.com>
2025-04-30 11:29:42 +0200

    Update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true, // peut être retiré si warning
   images: {
-    domains: [], // ajoute ici les domaines autorisés si nécessaire
-  },
-  experimental: {
-    appDir: false,
-  },
-};
+    domains: []
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/components/BlogList.tsx
+++ src/components/BlogList.tsx
@@ -2,7 +2,7 @@
 import { motion } from 'framer-motion'
 import PostCard from './PostCard'
 
-export interface Post {
+interface Post {
   id: string
   title: string
   date: string
